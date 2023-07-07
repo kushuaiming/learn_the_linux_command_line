b5a0e6c1a135279cc21f326debc781c174f42b29
kushuaiming <xm2534911078@outlook.com>
2023-07-07 21:50:27 +0800

    feat: controlling processes

---
+++ 10_processes/controlling_processes.sh
@@ -0,0 +1,8 @@
+# The xlogo program is a sample supplied with the X Window system.
+xlogo
+# Launch a program which placed in the background.
+xlogo &
+# List the jobs that have been launched from the terminal.
+jobs
+# Return a process to the foreground.
+fg %1
